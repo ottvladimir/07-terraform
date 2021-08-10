0403574d0d8bf1e1350bbe3d0e983234da8ba7b2
ottvladimir <ottvvl@gmail.com>
2021-08-10 01:09:53 +0000

    modified:   07-terraform-02-syntax/terraform/main.tf

--- 07-terraform-02-syntax/terraform/main.tf
+++ 07-terraform-02-syntax/terraform/main.tf
@@ -1,17 +1,33 @@
-    provider "aws" {
-      region = "us-west-2"
-    }
+provider "aws" {
+        region = "us-west-2"
+      }
 
-    data "aws_ami" "ubuntu" {
-      owners = ["amazon"]
-      most_recent = true
-      filter {
-            name   = "name"
-            values = ["*ubuntu*"]
-            }
-    }
-    resource "aws_instance" "web" {
-        ami = "${data.aws_ami.ubuntu.id}"
-        instance_type = "t2.micro"
-	count=2
-    }
+      data "aws_ami" "ubuntu" {
+        owners = ["amazon"]
+        most_recent = true
+        filter {
+              name   = "name"
+              values = ["*ubuntu*"]
+              }
+      }
+      resource "aws_instance" "web" {
+          ami = "${data.aws_ami.ubuntu.id}"
+          instance_type = "t2.micro"
+          count = 2
+          ipv6_address_count = 1
+          cpu_core_count = 1
+          disable_api_termination = false
+	  ebs_block_device {
+            device_name = "/dev/xvdb"
+            volume_type = "gp2"
+            volume_size = 10
+          }
+        tags = {
+          "Name" = "HelloWorld"
+          }
+          user_data = <<EOF
+#!/bin/bash
+echo "Hello, world!"
+EOF
+      }
+
